c941abbcd2ab8ffb3fe64f232158adb4094676be
PratikshaJain37 <pratikshajain123@gmail.com>
2020-05-30 15:51:38 +0530

    try2

--- personal_portfolio/wsgi.py
+++ personal_portfolio/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xxxx.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settings')
-
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
